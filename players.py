e1dfe4afd5997d250879833a5e779b8b439ea4f8
NateDauterman <NateDauterman@users.noreply.github.com>
2015-12-16 11:42:51 -0500

    Update players.py

--- players.py
+++ players.py
@@ -60,10 +60,8 @@ Team Nate:
             else:
                 return 'b'
         if history[-1]=='b':
-            if opponent_history[-1]=='c':
-                return 'b'
-            else:
-                return 'b'
+            return 'b'
+            
 Team Heather:
         if getting_team_name:
             return 'Team Heather'
